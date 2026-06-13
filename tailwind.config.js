bbfa8a9fdb5198c55e93a4e1999f2f887ceed294
chaitanya1777549 <jankalachaitanya.24.csd@nits.edu.in>
2026-06-13 19:49:10 +0530

    Fix tailwind config variable name

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /**@type {import('tailwindcss').Config} */
 
-const Config = {
+const config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
